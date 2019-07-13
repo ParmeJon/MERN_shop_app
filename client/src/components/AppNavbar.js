46fb6082e0e646bfba7fea6ee381157cc4a8991f
github username <jonathansychan@gmail.com>
2019-07-13 18:56:40 -0400

    Adds Login modal and proper Axios with tokenConfig

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   Collapse,
   Navbar,
@@ -10,7 +10,10 @@ import {
   Container
 } from 'reactstrap';
 import RegisterModal from './auth/RegisterModal';
+import PropTypes from 'prop-types';
 import Logout from './auth/Logout';
+import Login from './auth/LoginModal';
+import { connect } from 'react-redux';
 
 class AppNavbar extends Component {
     // since we took out binding we don't really need constructor.
@@ -28,6 +31,10 @@ class AppNavbar extends Component {
       isOpen: false
     }
 
+    static propTypes = {
+      auth: PropTypes.object.isRequired
+    }
+
     toggle = () => {
       this.setState({
         isOpen: !this.state.isOpen
@@ -35,6 +42,34 @@ class AppNavbar extends Component {
     }
 
     render() {
+      const { isAuthenticated, user } =  this.props.auth
+
+      const authLinks = (
+        <Fragment>
+          <NavItem>
+            <span className="navbar-text mr-3">
+              <strong>{ user ? `Welcome ${user.name}` : ''}</strong>
+            </span>
+          </NavItem>
+          <NavItem>
+            <Logout />
+          </NavItem>
+        </Fragment>
+      );
+
+      const guestLinks = (
+        <Fragment>
+
+          <NavItem>
+            <RegisterModal />
+          </NavItem>
+          <NavItem>
+            <Login />
+          </NavItem>
+
+        </Fragment>
+      );
+
       return (
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
@@ -43,15 +78,11 @@ class AppNavbar extends Component {
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
+              { isAuthenticated ? authLinks : guestLinks }
                 <NavItem>
                   <NavLink href="https://github.com/parmejon">Github</NavLink>
                 </NavItem>
-                <NavItem>
-                  <RegisterModal />
-                </NavItem>
-                <NavItem>
-                  <Logout />
-                </NavItem>
+
               </Nav>
             </Collapse>
           </Container>
@@ -63,4 +94,8 @@ class AppNavbar extends Component {
 
 }
 
-export default AppNavbar;
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, null)(AppNavbar);
